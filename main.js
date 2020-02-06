9dfd5426319e15568c65a806772f92ae05b17d31
Breyanna Vass <breyannavass@gmail.com>
2020-02-06 13:47:02 -0500

    made an animate function

--- main.js
+++ main.js
@@ -17,3 +17,11 @@ const geometry = new THREE.BoxGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+}
