f3d935f777d29243fb32393319de9140d13dee99
Breyanna Vass <breyannavass@gmail.com>
2020-02-06 13:50:05 -0500

    made the lines look sharper

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
