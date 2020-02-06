78df3cb09f471b4da02319efe9ba4a184247890d
Breyanna Vass <breyannavass@gmail.com>
2020-02-06 13:39:40 -0500

    adding renderer and making it apart of the DOM

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
