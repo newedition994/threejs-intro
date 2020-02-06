f5c1e83cf1763f5d5069cc70ac58dba90826dc95
Breyanna Vass <breyannavass@gmail.com>
2020-02-06 13:48:52 -0500

    the cube now displays and moves around

--- main.js
+++ main.js
@@ -23,5 +23,10 @@ camera.position.z = 5;
 function animate() {
   requestAnimationFrame(animate);
 
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
   renderer.render(scene, camera);
 }
+
+animate();
